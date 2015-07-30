7c5739727cf68ea6f94f3e2bae814e8acc19e63d
Chella Norvell <Herbert@Chellas-MacBook-Pro.local>
2015-07-30 14:37:56 -0500

    adding new scripts

---
+++ v3/Python/test_multiple_urls/mult_urls.py
@@ -0,0 +1,30 @@
+import urllib2
+import string
+# create a password manager
+password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+
+# Add the username and password.
+# If we knew the realm, we could use it instead of None.
+top_level_url = "http://crossbrowsertesting.com/api/v3/screenshots"
+password_mgr.add_password(None, top_level_url, USERNAME, AUTHKEY)
+
+handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+
+# create "opener" (OpenerDirector instance)
+opener = urllib2.build_opener(handler)
+
+# use the opener to fetch a URL
+opener.open("http://crossbrowsertesting.com/api/v3/screenshots")
+
+# Install the opener.
+# Now all calls to urllib2.urlopen use our opener.
+urllib2.install_opener(opener)
+
+
+
+with open("urls.txt") as f:
+    for line in f:
+        data = '{"browsers": ["Win8.1|Chrome30","Nexus9-And50|MblChrome39"], "url": '+ "\""+ string.strip(line) +"\"" + '}'
+	req = urllib2.Request(top_level_url, data, {'Content-Type': 'application/json'})
+	urllib2.urlopen(req)
+
